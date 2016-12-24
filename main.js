e1739020271dbd5aaaa73b200b664a665f6bb8b0
Elihu <ea_2305@hotmail.com>
2016-12-24 13:58:26 -0600

    Update electron to electron compiler

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-//var request = require('request');
+'use strict';
+
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
 
+app.on('ready', () => {
+  
 
-function createWindow () {
-  // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
   // Open the DevTools.
@@ -19,30 +18,23 @@ function createWindow () {
   //Devtron Plugn developers
   //require('devtron').install();
 
-  
   mainWindow.on('closed', function () {
     mainWindow = null
   })
-}
 
-app.on('ready', createWindow)
+});
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+app.on('window-all-closed', () => {
+  
+  if (process.platform !== 'darwin')
+    app.quit();
+
+});
+
+app.on('activate', () => {
+  
+  if (mainWindow === null) 
     createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  
+});
